7b9b62183afe1edffe7a47f728a0ad014cdf3e7c
Arms-like-trees <117699558+Arms-like-trees@users.noreply.github.com>
2023-03-07 12:37:22 -0600

    adds some of the questions

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,5 +13,16 @@ VALUES ('Sales Lead', 109999, 1),
 ('Account Manager', 159999, 3),
 ('Accountant', 124999, 3),
 ('Legal Team Lead', 229999, 4),
-('Lawyer', 179999, 4);
+('Lawyer', 199999, 4);
 
+INSERT INTO employees(first_names, last_names, role_id, manager_id)
+VALUES ('Jon', 'Smith', 1, NULL),
+('Greg', 'Nayr', 2, 1),
+('Caleb', 'Whitson', 3, NULL),
+('Jason', 'Adams', 7, NULL),
+('Big', 'Stout', 4, 3),
+('Little', 'Sout', 8, 4),
+('Big', 'Wusy', 5, NULL),
+('Chris', 'Mcfly', 6, 7);
+
+-- first name, lastname, role id, manager id
--- server.js
+++ server.js
@@ -1,8 +1,108 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const express = require('express');
+//const express = require('express');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+//const PORT = process.env.PORT || 3001;
+//const app = express();
 
-const cTable = require('console.table');
+// create the connection to database
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  });
+  connection.connect(function(err){
+      console.log(err)
+  });
+ 
+  
+
+//const cTable = require('console.table');
+
+function start () {
+    inquirer
+    .prompt([
+    //Main Menu
+    {
+        name:mainMenu,
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all Roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update employee role',
+            'Delete employee',
+            'Quit'
+        ]
+    }
+])
+.then((answers) => {
+    
+    // depending on user selection determines which case is used
+    switch (answers.mainMenu) {
+
+        case 'View all departments':
+            db.query('SELECT * FROM departments', (err, data) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.table(data);
+                    start();
+                }
+            })
+            break;
+
+        case 'View all Roles':
+            db.query('SELECT * FROM roles', (err, data) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.table(data);
+                    start();
+                }
+            })
+            break;
+
+        case 'View all employees':
+            db.query('SELECT * FROM employees', (err, data) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.table(data);
+                    start();
+                }
+            })
+            break;
+
+        case 'Add a department':
+            break;
+
+        case 'Add a role':
+            break;
+
+        case 'Add an employee':
+            break;
+
+        case 'Update employee role':
+            break;
+
+        case 'Delete employee':
+            break;
+
+        case 'Quit':
+            break
+    }
+})
+// take the choice
+// to select other function to make dateabase quereis
+// then console.table()
+}
+
+
+
+start();
